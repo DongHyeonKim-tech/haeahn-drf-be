20365eb426961d24325d37f7aa2bd50bfa4c2b9e
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2023-12-11 17:11:07 +0900

    [수정] 프로젝트 명 assessment_be -> haeahn_drf_be 변경

--- haeahn_drf_be/urls.py
+++ haeahn_drf_be/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for assessment_be project.
+URL configuration for haeahn_drf_be project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
