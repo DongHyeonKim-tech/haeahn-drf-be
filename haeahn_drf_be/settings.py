81d399bf05d32921805a3767729c147fe82e69c4
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2023-12-13 16:12:27 +0900

    [수정] static file 설정 재재재재재수정

--- haeahn_drf_be/settings.py
+++ haeahn_drf_be/settings.py
@@ -141,7 +141,7 @@ STATIC_DIR = os.path.join(BASE_DIR / 'static')
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    STATIC_DIR / 'static'
 ]
 
 # Default primary key field type
