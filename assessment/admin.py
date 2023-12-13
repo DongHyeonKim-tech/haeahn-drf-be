af3b022e17b3e68078d61574be044cc96ff6d66d
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2023-12-14 08:59:13 +0900

    [수정] 언어 한국어 변경 및 django admin 페이지 설정

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from assessment.models import TbBimCertificationComm, TbBimCertificationManager, TbBimCertificationTest
+from assessment.models import TbBimCertificationManager, TbBimCertificationTest, TbBimCertificationTestUser, TbBimCertificationTestUserQuestion
 
 # Register your models here.
 # admin site에 model 추가
-admin.site.register(TbBimCertificationComm)
-admin.site.register(TbBimCertificationManager)
-admin.site.register(TbBimCertificationTest)
+
+class manager_model(admin.ModelAdmin):
+    list_display = ['user_id', 'user_name', 'dept_cd', 'dept_name', 'is_executive', 'is_used', 'description', 'is_bim_team']
+
+class test_model(admin.ModelAdmin):
+    list_display = ('name','test_type', 'test_datetime', 'max_participants', 'registration_start_date', 'registration_end_date', 'is_opened', 'is_generated_question', 'result_open_on', 'is_result_open')
+    search_fields = ['name']
+    
+class test_user_model(admin.ModelAdmin):
+    list_display = ['test_id', 'user_id', 'registration_datetime', 'start_datetime', 'end_datetime', 'is_submitted', 'submitted_on', 'is_observer', 'is_canceled', 'is_exception']
+
+class test_user_question_model(admin.ModelAdmin):
+    list_display = ['test_id', 'user_id', 'question_id', 'category', 'content', 'question_group'] 
+
+admin.site.register(TbBimCertificationManager, manager_model)
+admin.site.register(TbBimCertificationTest, test_model)
+admin.site.register(TbBimCertificationTestUser, test_user_model)
+admin.site.register(TbBimCertificationTestUserQuestion, test_user_question_model)
--- haeahn_drf_be/settings.py
+++ haeahn_drf_be/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -139,9 +139,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    STATIC_ROOT
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
