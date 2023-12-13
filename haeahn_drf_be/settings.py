d223ccdc34b634bd7b54e586427c5cdd3c0c506f
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2023-12-13 10:03:53 +0900

    [수정] static file 재재설정

--- haeahn_drf_be/settings.py
+++ haeahn_drf_be/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATIC_DIR = os.path.join(BASE_DIR / 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static')
+    STATIC_DIR,
 ]
 
 # Default primary key field type
