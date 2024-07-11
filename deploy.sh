a9731bd17fd0651e81c413f5dc2c27ec809978d5
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-07-11 10:10:48 +0900

    [수정] 삭제 image명 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 CONTAINER_ID=$(docker ps --filter name=haeahn-drf-be -q)
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
-docker rmi haeahn-drf-be-web
+docker rmi haeahn-drf-be_web
 docker-compose up -d --build
